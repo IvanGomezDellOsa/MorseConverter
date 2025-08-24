da5425ba4e9cd150f0c744205a5ae2473f4eeb40
Ivan Gomez DellOsa <ivangomezdellosa@gmail.com>
2025-08-24 16:28:30 -0300

    Codigo Python de traductor completo y sin errores

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import re
 
-dictionary_morse = {
+DICTIONARY_MORSE = {
     'A': '.-',     'B': '-...',   'C': '-.-.',   'D': '-..',    'E': '.',      'F': '..-.',
     'G': '--.',    'H': '....',   'I': '..',     'J': '.---',   'K': '-.-',    'L': '.-..',
     'M': '--',     'N': '-.',     'O': '---',    'P': '.--.',   'Q': '--.-',   'R': '.-.',
@@ -13,15 +13,48 @@ dictionary_morse = {
     ' ': '/'
 }
 
+DICTIONARY_REVERSE = {value: key for key, value in DICTIONARY_MORSE.items()}  # All values become keys to invert the dictionary
 
 
-def morse_convert(input):
-    input = input.upper()
-    converter_result = ""
-    for letter in input:
-         converter_result += dictionary_morse[letter] + " "
-    return converter_result
+def is_valid_input(input_data):
+    pattern = r'^[A-Za-z0-9 .,?\'!/\(\)&:;=+\-_"$@]+$'
+    return bool(re.search(pattern, input_data))
 
-text_input = input("Enter text to convert: ")
-converted = morse_convert(text_input)
-print(converted)
+def normalize_morse_input(input_data):
+    return input_data.replace('/', ' / ').split()  # Normalize user input for Morse code by converting '/',' / ', or ' /' to ' / '
+
+def is_morse(input_data):
+    pattern = r'^[. \-/]+$'
+    if not re.search(pattern, input_data):
+        return False
+    code = normalize_morse_input(input_data)
+    if all(morse_units in DICTIONARY_REVERSE for morse_units in code):
+        return True
+    raise ValueError("Invalid Morse code.")
+
+def morse_to_text(input_code):
+    split_code = normalize_morse_input(input_code)
+    converted_text = ''
+    for code in split_code:
+        converted_text += DICTIONARY_REVERSE[code]
+    return converted_text
+
+def text_to_morse(text):
+    text = text.upper()
+    morse_result = ''
+    for char in text:
+        morse_result += DICTIONARY_MORSE[char] + ' '
+    return morse_result
+
+
+user_input = input("Enter text or Morse code to convert: ")
+if is_valid_input(user_input):
+    try:
+        if is_morse(user_input):
+            print(morse_to_text(user_input))
+        else:
+            print (text_to_morse(user_input))
+    except ValueError as e:
+        print(f'Error: {e}')
+else:
+    print("Error: Invalid input. Verify that you are not using special characters like ñ, ü, etc.")
